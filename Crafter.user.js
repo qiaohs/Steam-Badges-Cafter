908b1f376d84b9c072a533d4f61b9c048488c909
qiaohs <365960051@qq.com>
2018-06-07 09:07:13 +0800

    Update Crafter.user.js

--- Crafter.user.js
+++ Crafter.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name			Steam Badges Crafter
+// @name			Steam Auto Mass Craft Cards Badges in Bulk
 // @name:zh-CN			Steam一键批量合卡合徽章
 // @name:zh-TW			Steam一鍵批量合卡合徽章
-// @version	 		1.0
-// @description			(Steam Auto Mass Craft Cards Badges in Bulk) It will automatically use up your gamecard sets for crafting badges. You can control the which card sets and how many sets to craft by using it.
+// @version	 		1.1
+// @description			(Steam Auto Mass Craft Trading Cards Badges in Bulk) It will automatically use up your gamecard sets for crafting badges. You can control the which card sets and how many sets to craft by using it.
 // @description:zh-CN		这是一个自动合卡插件，可以指定徽章合成的数量和种类
 // @description:zh-TW		這是一個自動合卡挿件，可以指定徽章合成的數量和種類
 // @author			QHS
@@ -13,7 +13,7 @@
 // @grant        		GM_getValue
 // @supportURL			https://steamcn.com/t339531-1-1
 // @icon			http://pan.hriq.org/steam.green.ico
-// @namespace 			https://greasyfork.org/users/155548-黑山東雲光圈研究所
+// @namespace 			https://greasyfork.org/users/155548
 // @namespace 			https://steamcommunity.com/profiles/76561198132556503
 // ==/UserScript==
 var		timer_scan = GM_getValue("timer_scan", 1000),			//扫描卡组间隔 Interval: between badges scans（ms）[500+ recommended]
