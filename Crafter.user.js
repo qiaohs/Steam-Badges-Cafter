f54d8a90814d8f37eb0885a4d19472bb213fd1fc
BM_DY_AS_Lab <365960051@qq.com>
2020-01-05 12:43:24 +0800

    New Event Badges - <The Steam Awards - 2019>

--- Crafter.user.js
+++ Crafter.user.js
@@ -2,7 +2,7 @@
 // @name			Steam Auto Mass Craft Cards Badges in Bulk
 // @name:zh-CN			Steam一键批量合卡合徽章
 // @name:zh-TW			Steam一鍵批量合卡合徽章
-// @version	 		2.25
+// @version	 		2.26
 // @description			(Steam Auto Mass Craft Trading Cards Badges in Bulk) It will automatically use up your gamecard sets for crafting badges. You can control the which card sets and how many sets to craft by using it.
 // @description:zh-CN		这是一个自动合卡插件，可以指定徽章合成的数量和种类
 // @description:zh-TW		這是一個自動合卡挿件，可以指定徽章合成的數量和種類
@@ -15,8 +15,9 @@
 // @icon			http://pan.hriq.org/steam.green.ico
 // @namespace 			https://greasyfork.org/users/155548
 // @namespace 			https://steamcommunity.com/profiles/76561198132556503
+// @downloadURL none
 // ==/UserScript==
-var		sales=["245070","762800","876740","991980"],//Appid for sales cards
+var		sales=["245070","762800","876740","991980","1195670"],//Appid for sales cards
     timer_craft = GM_getValue("timer_craft", 500),
     timer_scan = GM_getValue("timer_scan", 1000),
     config_cap_level = GM_getValue("config_cap_level", 0),
