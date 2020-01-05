64da275c2d7bb73aafd1f3de218feef75f2b34ba
BM_DY_AS_Lab <365960051@qq.com>
2020-01-05 12:44:05 +0800

    Fix Bug (shows 99999)

--- Crafter.user.js
+++ Crafter.user.js
@@ -2,7 +2,7 @@
 // @name			Steam Auto Mass Craft Cards Badges in Bulk
 // @name:zh-CN			Steam一键批量合卡合徽章
 // @name:zh-TW			Steam一鍵批量合卡合徽章
-// @version	 		2.26
+// @version	 		2.27
 // @description			(Steam Auto Mass Craft Trading Cards Badges in Bulk) It will automatically use up your gamecard sets for crafting badges. You can control the which card sets and how many sets to craft by using it.
 // @description:zh-CN		这是一个自动合卡插件，可以指定徽章合成的数量和种类
 // @description:zh-TW		這是一個自動合卡挿件，可以指定徽章合成的數量和種類
@@ -1024,7 +1024,7 @@ font.mnotice {
                     var _badge = $J(html).find('.badge_current .badge_info_description>div:eq(1)');
                     var cards_sum=0,cards_num=0,after_stm;
 
-                    $J(html).find('.badge_detail_tasks>.badge_card_set_card').each(function() {
+                    $J(html).find('.badge_card_set_card').each(function() {
                         var count = $J(this).find('.badge_card_set_text_qty').text();
                         cards_num  += 1;
 
